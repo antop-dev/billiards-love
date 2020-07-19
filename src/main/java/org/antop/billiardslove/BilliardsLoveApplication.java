eb652b492bcd5f92db75693925b74c850d4bc65f
Antop <antop@kakao.com>
2020-07-20 01:19:18 +0900

    project init

---
+++ src/main/java/org/antop/billiardslove/BilliardsLoveApplication.java
@@ -0,0 +1,13 @@
+package org.antop.billiardslove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilliardsLoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilliardsLoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/antop/billiardslove/BilliardsLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.antop.billiardslove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilliardsLoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
