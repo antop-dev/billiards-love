33d84e7f181aed0f30adb2456af092af1ab8c1b1
kiomnd2 <kiomnd2@naver.com>
2020-07-30 08:48:20 +0900

    소스 위치 변경

--- src/main/front/App.vue
+++ src/main/front/App.vue
@@ -4,8 +4,6 @@
     <div>
       <router-view></router-view>
     </div>
-    <md-button v-on:click="moveHello">헬로</md-button>
-    <md-button v-on:click="moveWorld">월드</md-button>
   </div>
 </template>
 
@@ -16,14 +14,6 @@ export default {
   components: { AppHeader },
   data() {
     return {};
-  },
-  methods: {
-    moveHello: function () {
-      this.$router.push('/hello');
-    },
-    moveWorld: function () {
-      this.$router.push('/world');
-    },
   },
 };
 </script>
