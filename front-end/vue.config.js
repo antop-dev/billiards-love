fa571c2e17df70356de42d6949a6d5d774efa224
Antop <antop@kakao.com>
2020-11-13 12:05:40 +0900

    front-end master로 복구?

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@': path.join(__dirname, 'src/components'),
-            },
-        },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src/components'),
+      },
     },
+  },
 };
