30e9108a6aaad42f4c3d769c8887ae4229720dd7
Antop <antop@kakao.com>
2020-12-13 22:27:30 +0900

    상수, final 처리

--- back-end/src/main/java/org/antop/billiardslove/util/Aes256.java
+++ back-end/src/main/java/org/antop/billiardslove/util/Aes256.java
@@ -23,9 +23,17 @@ import java.util.Base64;
 @UtilityClass
 public class Aes256 {
     /**
-     * IV 길이
+     * IV(initialization vector) 길이
      */
     public static final int GCM_IV_LENGTH = 12;
+    /**
+     * <a href="https://docs.oracle.com/javase/8/docs/technotes/guides/security/StandardNames.html#Cipher">Cipher (Encryption) Algorithms</a>
+     */
+    public static final String TRANSFORMATION = "AES/GCM/NoPadding";
+    /**
+     * <a href="https://docs.oracle.com/javase/8/docs/technotes/guides/security/StandardNames.html#KeyGenerator">KeyGenerator Algorithms</a>
+     */
+    public static final String ALGORITHM = "AES";
 
     /**
      * AES256 암호화에 사용할 키를 생성한다.
@@ -34,10 +42,9 @@ public class Aes256 {
      */
     @SneakyThrows
     public String generateKey() {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        final KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
         keyGenerator.init(256);
-        byte[] encoded = keyGenerator.generateKey().getEncoded();
-        return Base64.getEncoder().encodeToString(encoded);
+        return Base64.getEncoder().encodeToString(keyGenerator.generateKey().getEncoded());
     }
 
     /**
@@ -51,9 +58,9 @@ public class Aes256 {
     public String encrypt(String plainText, String key) {
         byte[] decodedKey = Base64.getDecoder().decode(key);
         byte[] iv = iv();
-        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-        SecretKey keySpec = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
-        GCMParameterSpec gcmSpec = new GCMParameterSpec(128, iv);
+        final Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        final SecretKey keySpec = new SecretKeySpec(decodedKey, 0, decodedKey.length, ALGORITHM);
+        final GCMParameterSpec gcmSpec = new GCMParameterSpec(128, iv);
         cipher.init(Cipher.ENCRYPT_MODE, keySpec, gcmSpec);
         byte[] encrypted = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
         byte[] buffer = new byte[iv.length + encrypted.length];
@@ -75,22 +82,22 @@ public class Aes256 {
         byte[] decoded = Base64.getDecoder().decode(encodedText);
         byte[] iv = Arrays.copyOfRange(decoded, 0, GCM_IV_LENGTH);
         byte[] encrypted = Arrays.copyOfRange(decoded, GCM_IV_LENGTH, decoded.length);
-        SecretKey keySpec = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
-        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(128, iv);
+        final SecretKey keySpec = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+        final Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        final GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(128, iv);
         cipher.init(Cipher.DECRYPT_MODE, keySpec, gcmParameterSpec);
         byte[] decrypted = cipher.doFinal(encrypted);
         return new String(decrypted);
     }
 
     /**
-     * IV(initialization vector) 를 생성한다.
+     * IV(initialization vector)를 생성한다.
      *
      * @return 12바이트
      */
     private byte[] iv() {
         byte[] iv = new byte[GCM_IV_LENGTH];
-        SecureRandom random = new SecureRandom();
+        final SecureRandom random = new SecureRandom();
         random.nextBytes(iv);
         return iv;
     }
