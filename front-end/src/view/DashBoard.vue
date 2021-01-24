593351c57baf0f42f0deb7139638ac40a1c16db2
kiomnd2 <kiomnd2@naver.com>
2021-01-24 17:48:14 +0900

    대시보드 컨텐츠 리스트 구조화, 헤더 타이틀 입력

--- front-end/src/view/DashBoard.vue
+++ front-end/src/view/DashBoard.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <app-header></app-header>
-    <div class="board">
-      <board-contents @click.native="getDetail"></board-contents>
-      <board-contents></board-contents>
-      <board-contents></board-contents>
+    <app-header title="대시보드" menu-btn="true"></app-header>
+    <div class="board" v-for="content in contentsList" v-bind:key="content.id">
+      <board-contents
+        :title="content.title"
+        :isOpen="content.isOpen"
+        :rank="content.rank"
+        @click.native="getDetail(content.id)"
+      ></board-contents>
     </div>
   </div>
 </template>
@@ -13,10 +16,28 @@ import BoardContents from '@/dashboard/BoardContents';
 import AppHeader from '@/common/AppHeader';
 export default {
   name: 'DashBoard',
+  data: function() {
+    return {
+      contentsList: [
+        {
+          id: 1,
+          title: '8월 챔프전',
+          isOpen: true,
+          rank: 1,
+        },
+        {
+          id: 2,
+          title: '7월 챔프전',
+          isOpen: false,
+          rank: 2,
+        },
+      ],
+    };
+  },
   components: { BoardContents, AppHeader },
   methods: {
-    getDetail() {
-      this.$router.push('/game/info');
+    getDetail(id) {
+      this.$router.push({ path: '/game', query: { id } });
     },
   },
 };
