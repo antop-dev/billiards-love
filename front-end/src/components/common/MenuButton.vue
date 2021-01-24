53bca597d064bc9cb31b5389b1f28a334b45e807
kiomnd2 <kiomnd2@naver.com>
2021-01-24 17:47:25 +0900

    메뉴(햄버거) 버튼 생성

---
+++ front-end/src/components/common/MenuButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <md-button class="md-icon-button">
+      <span style="cursor:pointer;" class="material-icons">
+        menu
+      </span>
+    </md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+};
+</script>
+
+<style scoped></style>
