c8f62fd3e41b7537f12bc3a54c0d53d2c8069b70
Antop <antop@kakao.com>
2021-02-02 12:59:34 +0900

    Principal 인터페이스 추가 + 코드 정리

--- back-end/src/main/java/org/antop/billiardslove/config/SecurityConfig.java
+++ back-end/src/main/java/org/antop/billiardslove/config/SecurityConfig.java
@@ -2,9 +2,6 @@ package org.antop.billiardslove.config;
 
 import lombok.RequiredArgsConstructor;
 import org.antop.billiardslove.config.security.JwtAuthenticationFilter;
-import org.antop.billiardslove.config.security.JwtTokenProvider;
-import org.antop.billiardslove.jpa.repository.MemberRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,9 +11,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final JwtTokenProvider jwtTokenProvider;
-    private final MemberRepository memberRepository;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,11 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .headers().frameOptions().disable()
                 .and()
-                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Bean
-    JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter(jwtTokenProvider, memberRepository);
-    }
 }
--- back-end/src/main/java/org/antop/billiardslove/config/security/JwtAuthenticationFilter.java
+++ back-end/src/main/java/org/antop/billiardslove/config/security/JwtAuthenticationFilter.java
@@ -1,11 +1,10 @@
 package org.antop.billiardslove.config.security;
 
 import lombok.RequiredArgsConstructor;
-import org.antop.billiardslove.handler.MemberNotFountException;
-import org.antop.billiardslove.jpa.repository.MemberRepository;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -16,27 +15,21 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Component
 public class JwtAuthenticationFilter extends GenericFilterBean {
-
     private final JwtTokenProvider jwtTokenProvider;
-    private final MemberRepository memberRepository;
+    private final PrincipalProvider principalProvider;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
         // 헤더에서 JWT 를 받아옵니다.
         String token = getToken(request);
-
         // 유효한 토큰인지 확인합니다.
         if (StringUtils.isNotBlank(token) && jwtTokenProvider.validateToken(token)) {
             String subject = jwtTokenProvider.getSubject(token);
-            long memberId = Long.parseLong(subject);
-
-            memberRepository.findById(memberId).orElseThrow(() -> new MemberNotFountException(memberId));
-
-            // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
-            JwtAuthenticationToken auth = new JwtAuthenticationToken(memberId, token);
-
+            String principal = principalProvider.getPrincipal(subject);
+            // Principal 로 인증 토큰 생성
+            JwtAuthenticationToken auth = new JwtAuthenticationToken(Long.valueOf(principal), token);
             // SecurityContext 에 Authentication 객체를 저장합니다.
             SecurityContextHolder.getContext().setAuthentication(auth);
         }
@@ -46,4 +39,5 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     private String getToken(ServletRequest request) {
         return ((HttpServletRequest) request).getHeader(HttpHeaders.AUTHORIZATION);
     }
+
 }
