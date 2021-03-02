9cb604e6a291e2a469e6c51729874fc1ee1b65ff
Antop <antop@kakao.com>
2021-03-03 00:35:07 +0900

    누락된 대회 참가 여부 속성 추가

--- back-end/src/main/java/org/antop/billiardslove/service/MemberService.java
+++ back-end/src/main/java/org/antop/billiardslove/service/MemberService.java
@@ -6,8 +6,9 @@ public interface MemberService {
     /**
      * 회원 정보 조회
      *
-     * @param id 회원 아이디
+     * @param memberId 회원 아이디
      * @return 회원 정보
+     * @throws org.antop.billiardslove.exception.MemberNotFountException 회원을 찾을 수 없을 경우
      */
-    Member getMember(long id);
+    Member getMember(long memberId);
 }
