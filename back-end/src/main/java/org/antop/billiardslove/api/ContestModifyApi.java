127c79b7f59dd6f5a83118f072952d3b2be2dae0
jmDevBlog <systemdata227@gmail.com>
2021-03-18 04:34:49 +0900

    billiards-love-93 대회 정보 수정 API 개발

--- back-end/src/main/java/org/antop/billiardslove/api/ContestModifyApi.java
+++ back-end/src/main/java/org/antop/billiardslove/api/ContestModifyApi.java
@@ -1,12 +1,6 @@
 package org.antop.billiardslove.api;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 import org.antop.billiardslove.config.security.JwtAuthenticationToken;
 import org.antop.billiardslove.dto.ContestDto;
 import org.antop.billiardslove.jpa.entity.Contest;
@@ -17,9 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 @RequiredArgsConstructor
 @RestController
 public class ContestModifyApi {
@@ -27,7 +18,7 @@ public class ContestModifyApi {
 
     @Secured(JwtAuthenticationToken.ROLE_MANAGER)
     @PutMapping("/api/v1/contest/{id}")
-    public Response modify(@PathVariable(name = "id") long contestId, @RequestBody Request request) {
+    public ContestResponse modify(@PathVariable(name = "id") long contestId, @RequestBody ContestModifyRequest request) {
 
         ContestDto contestDto = ContestDto.builder()
                 .title(request.getName())
@@ -41,166 +32,23 @@ public class ContestModifyApi {
 
         Contest contest = contestService.modify(contestId, contestDto);
 
-        return Response.builder()
+        return ContestResponse.builder()
                 .id(contest.getId())
                 .name(contest.getTitle())
                 .description(contest.getDescription())
-                .start(Start.builder()
+                .start(ContestResponse.Start.builder()
                         .startDate(contest.getStartDate())
                         .startTime(contest.getStartTime())
                         .build())
-                .end(End.builder()
+                .end(ContestResponse.End.builder()
                         .endDate(contest.getEndDate())
                         .endTime(contest.getEndTime())
                         .build())
-                .state(State.builder()
+                .state(ContestResponse.State.builder()
                         .code(contest.getState().getCode())
                         .name(contest.getState().name())
                         .build())
                 .maximumParticipants(contest.getMaximumParticipants())
                 .build();
-    }
-
-    /**
-     * 대회 수정 요청
-     *
-     * @author jammini
-     */
-    @Getter
-    @NoArgsConstructor
-    @ToString
-    static class Request {
-        /**
-         * 대회명
-         */
-        private String name;
-        /**
-         * 대회 설명
-         */
-        private String description;
-        /**
-         * 시작 일시
-         */
-        private Start start;
-        /**
-         * 종료 일시
-         */
-        private End end;
-        /**
-         * 최대 참가 인원
-         */
-        private Integer maximumParticipants;
-
-        @Getter
-        static class Start {
-            /**
-             * 시작일
-             */
-            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMdd")
-            private LocalDate startDate;
-            /**
-             * 시작시간
-             */
-            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HHmmss")
-            private LocalTime startTime;
-        }
-
-        @Getter
-        static class End {
-            /**
-             * 종료일
-             */
-            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMdd")
-            private LocalDate endDate;
-            /**
-             * 종료시간
-             */
-            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HHmmss")
-            private LocalTime endTime;
-        }
-    }
-
-    /**
-     * 대회 수정 응답
-     *
-     * @author jammini
-     */
-    @Getter
-    @ToString
-    @AllArgsConstructor
-    @Builder
-    static class Response {
-        /**
-         * 대회 아이디
-         */
-        private final Long id;
-        /**
-         * 대회명
-         */
-        private final String name;
-        /**
-         * 대회 설명
-         */
-        private final String description;
-        /**
-         * 시작 일시
-         */
-        private final Start start;
-        /**
-         * 종료 일시
-         */
-        private final End end;
-        /**
-         * 진행 상태
-         */
-        private final State state;
-        /**
-         * 최대 참가 인원
-         */
-        private final Integer maximumParticipants;
-
-    }
-
-    @Getter
-    @Builder
-    static class Start {
-        /**
-         * 시작일
-         */
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMdd")
-        private final LocalDate startDate;
-        /**
-         * 시작시간
-         */
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HHmmss")
-        private final LocalTime startTime;
-    }
-
-    @Getter
-    @Builder
-    static class End {
-        /**
-         * 종료일
-         */
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyyMMdd")
-        private final LocalDate endDate;
-        /**
-         * 종료시간
-         */
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HHmmss")
-        private final LocalTime endTime;
-    }
-
-    @Getter
-    @Builder
-    static class State {
-        /**
-         * 상태 코드
-         */
-        private final String code;
-        /**
-         * 상태명
-         */
-        private final String name;
     }
 }
