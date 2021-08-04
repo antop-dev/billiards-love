1a8a8b6b92577c1e63bd42e22208053e3d7e4c5c
Antop <antop@kakao.com>
2021-08-05 00:16:36 +0900

    billiards-love-132 Jaspyt를 이용한 application.properties 값 암호화

---
+++ back-end/src/test/java/org/antop/billiardslove/JasyptTest.java
@@ -0,0 +1,34 @@
+package org.antop.billiardslove;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Jasypt 라이브러리는 이용해서 값을 암호화 하는 클래스다.
+ *
+ * @author antop
+ */
+public class JasyptTest {
+    /**
+     * 암복호화에 사용되는 키
+     */
+    private final static String PASSWORD = "";
+    /**
+     * 암호화 하려는 값<br>
+     * 암호화 한 값은 application.properties 에서 "ENC(암호화된 값)"으로 사용된다.
+     */
+    private final static String[] PLAIN_TEXTS = {""};
+
+    @Test
+    void generate() {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(PASSWORD);
+
+        for (String plainText : PLAIN_TEXTS) {
+            String encrypted = encryptor.encrypt(plainText);
+            System.out.println(plainText + " -> " + encrypted);
+        }
+
+
+    }
+}
