6582d22cd92c4b0e7ad700cfc7cf00385a4424a9
kiomnd2 <kiomnd2@gmail.com>
2021-09-06 01:59:31 +0900

    Vuetify로 변경, 스타일 변경

--- front-end/src/components/common/AppHeader.vue
+++ front-end/src/components/common/AppHeader.vue
@@ -1,42 +1,19 @@
 <template>
-  <md-toolbar class="md-primary">
-    <div class="md-toolbar-row">
-      <div class="md-toolbar-section-start">
-        <back-button v-if="backBtn"></back-button>
-        <menu-button v-if="menuBtn"></menu-button>
-      </div>
-      <div class="md-layout-item title-align-center">
-        <span class="md-title">{{ title }}</span>
-      </div>
-      <div class="md-toolbar-section-end text-align-right">
-        <close-button v-if="closeBtn"></close-button>
-      </div>
-    </div>
-  </md-toolbar>
+  <v-card>
+    <v-app-bar app fixed elevate-on-scroll color="deep-purple accent-4" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>대시보드</v-toolbar-title>
+    </v-app-bar>
+  </v-card>
 </template>
 
 <script>
-import CloseButton from './CloseButton';
-import BackButton from './BackButton';
-import MenuButton from './MenuButton';
-
 export default {
   name: 'AppHeader',
   props: {
     title: String,
-    closeBtn: String,
-    backBtn: String,
-    menuBtn: String,
   },
-  components: { CloseButton, BackButton, MenuButton },
 };
 </script>
 
-<style>
-.title-align-center {
-  text-align: center;
-}
-.text-align-right {
-  text-align: right;
-}
-</style>
+<style></style>
